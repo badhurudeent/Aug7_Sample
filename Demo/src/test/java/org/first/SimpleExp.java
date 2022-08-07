072172302eba5b4c09cac5e497c1618d42914109
badhurudeens <100840567+badhurudeens@users.noreply.github.com>
2022-08-07 12:24:25 +0530

    Update SimpleExp.java
    55 line added

--- Demo/src/test/java/org/first/SimpleExp.java
+++ Demo/src/test/java/org/first/SimpleExp.java
@@ -5,7 +5,7 @@ public class SimpleExp {
 	public static void main(String[] args) {
 		System.out.println("30");
 		System.out.println("44");
-		System.out.println("5");
+		System.out.println("55");
 	}
 
 }
