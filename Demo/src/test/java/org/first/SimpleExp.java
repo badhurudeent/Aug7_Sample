fc09e4cee4ec3be49a3ed887f082540cdb82ca95
badhurudeens <100840567+badhurudeens@users.noreply.github.com>
2022-08-07 12:04:05 +0530

    Update SimpleExp.java
    30 added in line number 6

--- Demo/src/test/java/org/first/SimpleExp.java
+++ Demo/src/test/java/org/first/SimpleExp.java
@@ -3,7 +3,7 @@ package org.first;
 public class SimpleExp {
 
 	public static void main(String[] args) {
-		System.out.println("3");
+		System.out.println("30");
 		System.out.println("4");
 		System.out.println("5");
 	}
